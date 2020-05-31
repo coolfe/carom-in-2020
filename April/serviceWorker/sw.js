f841226d687ac15f8e2f1a98f595f0b26dc21761
coolfe <coolfe36@gmail.com>
2020-05-31 22:40:48 +0800

    fix: update Worker

--- April/serviceWorker/sw.js
+++ April/serviceWorker/sw.js
@@ -1,7 +1,15 @@
-window.addEventListener('install', e => {
+self.addEventListener('install', e => {
   e.waitUntil(
     caches.open('my-cache').then(cache => {
       return cache.addAll(['./index.html', './index.js'])
     })
   )
 })
+self.addEventListener('activate', function (event) {
+  // ...
+});
+
+// 监听主文档中的网络请求
+self.addEventListener('fetch', function (event) {
+  // ...
+});
--- April/worker/worker.js
+++ April/worker/worker.js
@@ -1,4 +1,6 @@
 self.onmessage = function(event) {
+  // 它的 data 属性包含主线程发来的数据。
+  // self.postMessage() 方法用来向主线程发送消息
   let data = event.data;
   this.postMessage(data);
 }
